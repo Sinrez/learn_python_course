b603eac60773b57dba20fb64394212a4b09120de
sintez <madsint@gmail.com>
2023-02-15 18:37:54 +0300

    new solutions

--- mybot/test_ephem.py
+++ mybot/test_ephem.py
@@ -1,6 +1,14 @@
 import ephem
+import datetime
 # python3 test_ephem.py
 
-mars = ephem.Mars('2000/01/01')
-const = ephem.constellation(mars)
-print(const)
+# mars = ephem.Mars('2000/01/01')
+# const = ephem.constellation(mars)
+# print(const)
+
+today = datetime.datetime.now()
+planet = getattr(ephem, 'Mars')(today.strftime('%Y/%m/%d'))
+# https://docs-python.ru/tutorial/vstroennye-funktsii-interpretatora-python/funktsija-getattr/
+ephem_answer = ephem.constellation(planet)
+print(*ephem_answer)
+print(ephem_answer[1])
