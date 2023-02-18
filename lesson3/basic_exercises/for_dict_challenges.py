d168a25e465317d4ebdd1f6888e7ec6508857a3a
sintez <madsint@gmail.com>
2023-02-18 15:13:45 +0300

    new solutions

--- lesson3/basic_exercises/for_dict_challenges.py
+++ lesson3/basic_exercises/for_dict_challenges.py
@@ -1,3 +1,4 @@
+import collections
 # Задание 1
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика
 # Пример вывода:
@@ -12,7 +13,15 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+dict_res = {}
+for s in students:
+    if dict_res.get(s['first_name'], None):
+        dict_res[s['first_name']] += 1
+    else:
+        dict_res[s['first_name']] = 1
+for k, v in dict_res.items():
+    print(f'{k}: {v}')
+print(10 * '*')
 
 
 # Задание 2
@@ -26,8 +35,18 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
-
+dict_name = {}
+for s in students:
+    if dict_name.get(s['first_name'], None):
+        dict_name[s['first_name']] += 1
+    else:
+        dict_name[s['first_name']] = 1
+max_v = max(dict_name.values())
+# print(max_v)
+for k, v in dict_name.items():
+    if v == max_v:
+        print(f'Самое частое имя среди учеников: {k}')
+print(10 * '*')
 
 # Задание 3
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
@@ -51,7 +70,23 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+
+def dict_fname(st):
+    dict_name = {}
+    for s in st:
+        if dict_name.get(s['first_name'], None):
+            dict_name[s['first_name']] += 1
+        else:
+            dict_name[s['first_name']] = 1
+    max_v = max(dict_name.values())
+# print(max_v)
+    for k, v in dict_name.items():
+        if v == max_v:
+            return k
+
+for i in range(len(school_students)):
+    print(f'Самое частое имя в классе {i+1}: {dict_fname(school_students[i])}')
+print(10 * '*')
 
 
 # Задание 4
@@ -72,7 +107,20 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
+
+def get_nums_girls_and_boys(students):
+    gils, boys = 0, 0
+    for one_student in students:
+        if is_male[one_student["first_name"]]: 
+            boys += 1
+        else: 
+            gils += 1
+    return(gils, boys)
+
+for one_class in school:
+    girls,boys = get_nums_girls_and_boys(one_class["students"])
+    print(f'В классе {one_class["class"]} {girls} девочки и {boys} мальчика.')
+print(10 * '*')
 
 
 # Задание 5
@@ -91,5 +139,22 @@ is_male = {
     'Олег': True,
     'Миша': True,
 }
-# ???
 
+genders = {}
+for one_class in school:
+    girls,boys = get_nums_girls_and_boys(one_class["students"])
+    genders[one_class['class']] = [girls, boys]
+
+girl_max = []
+boys_max = []
+for k, v in genders.items():
+    girl_max.append(genders[k][0])
+    boys_max.append(genders[k][1])
+gerls_val_max = max(girl_max)
+boys_val_max = max(girl_max)
+
+for k, v in genders.items():
+    if genders[k][0] == gerls_val_max:
+        print(f"Больше всего девочек в классе {k}")
+    if genders[k][1] == boys_val_max:
+        print(f"Больше всего мальчиков в классе {k}")
