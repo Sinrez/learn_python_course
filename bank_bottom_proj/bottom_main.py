3c9e393a1880d12a9648f0b53256b81ca2a1c095
sintez <madsint@gmail.com>
2023-03-01 15:48:23 +0300

    new solutions

--- bank_bottom_proj/bottom_main.py
+++ bank_bottom_proj/bottom_main.py
@@ -1,42 +1,62 @@
 # https://www.banki.ru/services/responses/list/?rate[]=1&rate[]=2
 # https://www.banki.ru/services/responses/list/?page=2&is_countable=on&rate[]=1&rate[]=2
+# href="/services/responses/bank/response/
 # source env_bottom/bin/activate
 
-import requests as re
+import requests as req
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import os
-import json
-import html
-import lxml.html
 
 
-ua = UserAgent()
-fake_ua = {'user-agent': ua.random}
+url_1 = 'https://www.banki.ru/services/responses/list/?rate[]=1&rate[]=2'
+url_max = 'https://www.banki.ru/services/responses/list/?page=1440&is_countable=on&rate[]=1&rate[]=2' # c начала 2022.
 
-url_in = 'https://www.banki.ru/services/responses/list/?rate[]=1&rate[]=2'
-resp = re.get(url=url_in,  headers=fake_ua)
-with open('res.txt','w') as fl:
-    fl.write(resp.text)
-# print(resp.text)
+url_from_parse = []
 
-bs = BeautifulSoup(resp.text, 'html.parser')
-bs_res_1 = bs.find('script', type="application/ld+json")
-with open('bs.txt','w') as fl:
-    fl.write(bs_res_1.text)
-print(type(bs_res_1))
-print(json.loads(bs_res_1.text))
-# print(*bs_res_1.children)
-# for b in bs_res_1.children:
-#     print(b)
-
-
-#заготовка
-def parser():
+def urls_parser(url_in):
     url_base_1 = 'https://www.banki.ru'
+    ua = UserAgent()
+    fake_ua = {'user-agent': ua.random}
+
+    resp = req.get(url=url_in,  headers=fake_ua)
+    bs = BeautifulSoup(resp.text, 'lxml')
+    bs_res_1 = bs.find_all('a', attrs={'class':"link-simple"})
+    for b in bs_res_1:
+        href = b.attrs.get("href")
+        if '/services/responses/bank/response/' in href:
+            url_from_parse.append(url_base_1+href)
+
+def page_parser(url_page):
+    ua = UserAgent()
+    fake_ua = {'user-agent': ua.random}
+
+    resp_fe = req.get(url=url_page,  headers=fake_ua)
+    bs2 = BeautifulSoup(resp_fe.text, 'html.parser')
+
+    #получаем краткий отзыв - название отзыва
+    short_fee = bs2.find_all('h1', class_ = "text-header-0 le856f50c")
+    short_feedback = str([s.get_text() for s in short_fee][0]).strip()
+    #получаем название банка 
+    bank_na = bs2.find('img', class_='lazy-load')
+    bank_name = str(bank_na).split()[1].replace('alt="','').replace('"','')
+    #получаем полный отзыв
+    resp = bs2.find('div', class_='lb1789875 markdown-inside markdown-inside--list-type_circle-fill')
+    response_full = [s.get_text() for s in resp][1]
+    #получаем дату отзыва replace("['\n', ","").replace("'\n']","")
+    dt = bs2.find('span', class_='l10fac986')
+    response_date = [d.get_text().strip() for d in dt][0]
+    #получаем город отзывы
+    ct = bs2.find('span', class_='l3a372298')
+    response_city = [c.get_text().strip() for c in ct][0]
+
+    return url_page, bank_name, short_feedback, response_date, response_city, response_full
+
 
 if __name__ == '__main__':
-    pass
+    urls_parser(url_1)
+    for url in url_from_parse:
+        print(page_parser(url))
