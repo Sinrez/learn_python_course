fcc5f8bff76e1596ce21f41f1ba2962c79105c35
sintez <madsint@gmail.com>
2023-03-02 16:47:46 +0300

    new solutions

--- web_flask/flask_server.py
+++ web_flask/flask_server.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from weather import weather_by_city
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Привет!"
 
+@app.route("/weather")
+# http://127.0.0.1:5000/weather
+def weather():
+    weather = weather_by_city("Moscow,Russia")
+    if weather:
+        return f"Сейчас {weather['temp_C']},ощущается как {weather['FeelsLikeC']}"
+    else:
+        return "Прогноз сейчас недоступен"
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
---
+++ web_flask/weather.py
@@ -0,0 +1,25 @@
+import requests
+from api_key import API_KEY
+
+def weather_by_city(city_name):
+    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    params = {
+        "key": API_KEY,
+        "q": city_name,
+        "format": "json",
+        "num_of_days": 1,
+        "lang": "ru"
+    }
+    result = requests.get(weather_url, params=params)
+    weather = result.json()
+    if 'data' in weather:
+        if 'current_condition' in weather['data']:
+            try:
+                return weather['data']['current_condition'][0]
+            except(IndexError, TypeError):
+                return False
+    return False
+
+if __name__ == "__main__":
+    weather = weather_by_city("Moscow,Russia")
+    print(weather)
