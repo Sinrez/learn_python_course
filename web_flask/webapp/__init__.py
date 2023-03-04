903436f267c9f80c9a432a6df611cb46bd5e935c
sintez <madsint@gmail.com>
2023-03-04 15:57:58 +0300

    work with flask

--- web_flask/webapp/__init__.py
+++ web_flask/webapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from webapp.weather import weather_by_city
 from webapp.bs_ex import get_python_news
 from webapp.model import db, News
+from webapp.forms import LoginForm
 
 
 def create_app():
@@ -13,11 +14,18 @@ def create_app():
     # with app.app_context():
     #     db.create_all()
 
+    @app.route('/login')
+    def login():
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
     @app.route("/")
     def weather():
         title = 'Новостной сайт'
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         news = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news)
+    
     return app
 
